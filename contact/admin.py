fe61350448a6a49fb0bfa9fe8636eb138cb039f8
Otavio Sousa <otaviosousamartins14@gmail.com>
2025-01-20 21:45:42 -0300

    Refactor contact views and templates; add contact detail view and update index page layout

--- contact/admin.py
+++ contact/admin.py
@@ -8,8 +8,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name', 'last_name', 'phone')
+    list_display = ('id', 'first_name', 'last_name', 'phone' , 'show')
     ordering = ('-id',)
     search_fields = ('first_name', 'last_name', 'email')
+    list_editable = ('show',)
     list_per_page = 10
     list_max_show_all = 200
